964110adbabb23079a17664fcc01510f7b8c900f
flyfish <two-finger@163.com>
2020-10-15 19:07:09 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,21 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('title', 'INMIX 在线配镜')</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
