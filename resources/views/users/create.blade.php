3ab1892a7d52366c66d69eb1873a7c7bfdc83954
flyfish <two-finger@163.com>
2020-10-16 15:09:09 +0800

    完成用户注册功能

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,7 @@
                 <h5>注册</h5>
             </div>
             <div class="card-body">
+                @include('shared._errors')
                 <form action="{{ route('users.store') }}" method="POST" role="form">
                     {{ csrf_field() }}
                     <div class="form-group">
@@ -26,8 +27,8 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="password-confirmation">确认密码</label>
-                        <input type="password" name="password-confirmation" class="form-control" value="{{ old('password-confirmation') }}" />
+                        <label for="password_confirmation">确认密码</label>
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}" />
                     </div>
 
                     <button type="submit" class="btn btn-primary">Submit</button>
